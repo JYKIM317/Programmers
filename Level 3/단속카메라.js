0fb8c3628b03b3b85604e854aa5353ba098e0a1b
Jinyoung Kim <83767872+JYKIM317@users.noreply.github.com>
2024-12-11 00:25:41 +0900

    Update 단속카메라.js

--- Level 3/단속카메라.js
+++ Level 3/단속카메라.js
@@ -2,10 +2,12 @@ function solution(routes) {
     let camera = -30001;
     routes.sort((a, b) => a[1] - b[1]);
     const cameraCount = routes.reduce((count, route) => {
-        if(route[0] > camera) {
+        const min = Math.min(route[0], route[1]);
+        if(min > camera) {
             count++;
             camera = route[1];
         }
+        
         return count;
     }, 0);
     
